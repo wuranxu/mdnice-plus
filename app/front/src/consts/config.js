c29a077aa9d5330220e2f15fbe3286c267d87191
wuranxu <619434176@qq.com>
2022-04-01 18:38:26 +0800

    优化

--- app/front/src/consts/config.js
+++ app/front/src/consts/config.js
@@ -1,5 +1,5 @@
 export default {
-  ServiceUrl: "http://121.5.2.74:8888",
+  ServiceUrl: "http://article.pity.fun",
   // ServiceUrl: "http://localhost:8888",
   ICON_URL: "//at.alicdn.com/t/font_915840_i1as8fy8kj.js"
 }
